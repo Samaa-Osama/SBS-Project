69a23951f007756f5c9fab8ef9ac95faa9c831fa
Nader Zakaria <102794875+naderZ2@users.noreply.github.com>
2024-04-14 23:53:52 +0200

    Update NavBarJavaS.js

--- js/NavBarJavaS.js
+++ js/NavBarJavaS.js
@@ -2,7 +2,7 @@ document.addEventListener('DOMContentLoaded', (event) => {
     var navbarHTML = `
     <nav class="navbar">
     <a href="HomePage.html">
-    <img src="../img/SBSLOGO.png" alt="SBS Logo" class="logo">
+    <img src="/img/SBSLOGO.png" alt="SBS Logo" class="logo">
     </a>
         <div class="menu">
             <span></span>
