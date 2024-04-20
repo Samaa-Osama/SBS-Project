7bd6d1de06aa89a0f1596838e584ec9e89623a8c
salahalkashif <164943134+salahalkashif@users.noreply.github.com>
2024-04-20 19:19:50 +0200

    Update NavBarJavaS.js

--- js/NavBarJavaS.js
+++ js/NavBarJavaS.js
@@ -25,6 +25,27 @@ document.addEventListener('DOMContentLoaded', (event) => {
 
     var footerhtml = `
     <footer>
+    <p>Contact Us!</p>
+    <div class="links">
+ <p>
+    <a href="sbsstudentactivity@gmail.com" target="_blank" title="SBS Student Activity Gmail Link">SBS Gmail</a>
+</p>
+<p>
+    <a href="https://www.facebook.com/SBS.Student.Activity?mibextid=ZbWKwL" target="_blank" title="SBS Student Activity Facebook Page Link">SBS Facebook Page</a>     
+</p>
+<p>
+    <a href="https://www.facebook.com/groups/716098337079680/?ref=share" target="_blank" title="SBS Student Activity Facebook Groub Link">SBS Facebook Groub</a>
+</p>
+<p>
+    <a href="https://www.linkedin.com/company/sbs-student-activity/" target="_blank" title="SBS Student Activity LinkedIn Link">SBS LinkedIn Account</a>
+</p>
+<p>
+    <a href="https://www.tiktok.com/@sbs.student.activity?_t=8lSRVSqM5m1&_r=1" target="_blank" title="SBS Student Activity TikTok Link">SBS TikTok Account</a>
+</p>
+<p>
+    <a href="https://www.instagram.com/sbs_student_activity?igsh=MzRlODBiNWFlZA==" target="_blank" title="SBS Student Activity Instagram Link"> SBS Instagram Account</a>
+</p>
+</div>
         <p>&copy; 2024 SBS. All rights reserved. | <a href="privacy.html" class="footerlink">Privacy Policy</a> | <a href="terms.html" class="footerlink">Terms of Service</a></p>
     </footer>
     `;
